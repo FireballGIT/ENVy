4629714ca113284d83108311173b9633d3c6ae15
FireballGIT at BAG Studios <gafutb@outlook.com>
2026-02-07 07:28:38 -0600

    Define __all__ for panel module exports

---
+++ panels/__init__.py
@@ -0,0 +1,12 @@
+__all__ = [
+  "home_panel",
+  "env_panel",
+  "system_panel",
+  "hotkeys_panel",
+  "processes_panel",
+  "hardware_panel",
+  "startup_panel",
+  "network_panel",
+  "logs_panel",
+  "settings_panel"
+]
